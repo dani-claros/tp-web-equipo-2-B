98406682d4d033e6063be53ae8ec5cdd7c294483
Dana <clarosdana03@gmail.com>
2025-09-30 01:14:12 -0300

    Se agrego class Voucher

---
+++ modelo/Voucher.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modelo
+{
+    public class Voucher
+    {
+        public string CodigoVoucher { get; set; } 
+
+        public DateTime? FechaCanje { get; set; }
+
+        // Se relaciona con Cliente
+        // Hace falta crear esa class
+        // public Cliente Cliente { get; set; }
+
+        // Se relaciona con Articulo
+        public Articulo Articulo { get; set; }
+    }
+}
