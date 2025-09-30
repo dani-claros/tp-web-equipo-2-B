1a2c0d1df9a25a407186e81f14e5850bb422bd0d
Dana <clarosdana03@gmail.com>
2025-09-30 01:28:35 -0300

    Se agrego int id a la class Articulo, y se eliminaron comentariosno necesarios

--- modelo/Articulo.cs
+++ modelo/Articulo.cs
@@ -8,20 +8,13 @@ namespace modelo
 {
     public class Articulo
     {
+        public int Id { get; set; }
         public int CodigoArticulo { get; set; }
         public string Nombre { get; set; }
         public string Descripcion { get; set; }
         public decimal Precio { get; set; }
-
-        //Marca class Marca
-       // public int IdMarca { get; set; } NO es necesario
         public Marca Marca { get; set; }
-
-        //Categoria class Categoria
-        public int IdCategoria { get; set; }//No es Necesario
         public Categoria Categoria { get; set; }
-
-        //Imagen class Imagen
         public List<Imagen> Imagenes { get; set; }
     }
 }
